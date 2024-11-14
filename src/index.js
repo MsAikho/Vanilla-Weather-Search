73b2a06e04e0f62821cd6b13cbdbba3f65dbbedd
MsAikho <stuurmanakhona4@gmail.com>
2024-11-14 11:57:42 +0200

    Add little of html and css and finishing javascript.html

--- src/index.js
+++ src/index.js
@@ -7,16 +7,17 @@ function updateWeather(response) {
   let windspeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  
-cityElement.innerHTML = response.data.city;
+  iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class ="weather-icon"/>`;
+
+  cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windspeedElement.innerHTML = ` ${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
-  
 
 function formatDate(date) {
   let hours = date.getHours();
